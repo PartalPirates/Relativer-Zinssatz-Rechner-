718d2af51b9e40df12bcc93c64a1e5cfd9342b16
A.Marw.D <80158791+PartalPirates@users.noreply.github.com>
2021-08-02 00:24:40 +0200

    Update Protzenrechner.py

--- Protzenrechner.py
+++ Protzenrechner.py
@@ -1,4 +1,4 @@
-
+#hier kommt der effektive Zinssatz nach der Rechnung 
 Angebote=[]
 
 def effektiv(Zinssatz,Gebühren,Kapital,Tage):
@@ -21,7 +21,9 @@ def effektiv(Zinssatz,Gebühren,Kapital,Tage):
     effektiver_Zinssatz=Kreditkosten*100*365/(ausgezahltes_Kaptital*Tage)
     Antwort="Der Effektive Zinssatz liegt bei {:.3f} % ".format(effektiver_Zinssatz)
     List.append(Antwort)
-
+    
+    
+#Hier schreibt man wie viele Angebote man vergleichen möchte
 Anzahl=int(input("Anzahl der Angebote:"))
 for i in range(2):
     Zinssatz=float(input("Gib ein Zinssatz ein:"))
@@ -35,8 +37,7 @@ print("Das günstigere Angebot ist ",min(List))
 
 
 
-
-
+# mit freundlichen Grüßen ֎֎ 
 
 
 
